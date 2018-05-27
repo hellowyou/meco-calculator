90999e646bd9f4ea4c291c3db9d9042b0b5c6288
Vi Tu <virnont@gmail.com>
2018-05-27 17:08:05 +0800

    add formula module add,update,remove actions and reducers

--- src/redux/modules/formulas.js
+++ src/redux/modules/formulas.js
@@ -7,11 +7,40 @@ export default new Duck({
         items: [],
     },
     reducer: (state, action, duck) => {
+        const { actionResolvers } = duck.options;
+
         switch(action.type) {
+            case duck.types.ADD:
+                return actionResolvers.addFormulaItem(state, action.payload, duck);
+            case duck.types.UPDATE:
+                return actionResolvers.updateFormulaItem(state, action.payload, duck);
+            case duck.types.REMOVE:
+                return actionResolvers.removeFormulaItem(state, action.payload, duck);
             default: return state;
         }
     },
-    selectors: {
+    actionResolvers: {
+        addFormulaItem: (state, formulaItem, duck) =>
+            ({...state, items: [...state.items, formulaItem ]}),
+        updateFormulaItem: (state, formulaItem, duck) => ({
+            ...state,
+            items: state.items.map((item) => {
+                if(formulaItem.id !== item.id) {
+                    return item;
+                };
+    
+                return {
+                    ...item,
+                    ...formulaItem,
+                    id: item.id,
+                }
+            })
+        }),
+        removeFormulaItem: (state, formulaItemId, duck) => 
+            ({...state, items: state.items.filter(item => item.id !== formulaItemId)}),
+    },
+    selectors: (duck) => ({
+        items: state => state[duck.store].items
         // Use with reselect
         // shopItems:  state => state.shop.items,
         // subtotal: new Duck.Selector(selectors =>
@@ -20,9 +49,11 @@ export default new Duck({
         //         items => items.reduce((acc, item) => acc + item.value, 0)
         //     )
         // )
-    },
+    }),
     creators: (duck) => ({
-        addFormulaItem: formulaItem => ({type: duck.types.ADD, formulaItem}),
+        addFormulaItem: formulaItem => ({type: duck.types.ADD, payload: formulaItem}),
+        updateFormulaItem: formulaItem => ({type: duck.types.UPDATE, payload: formulaItem}),
+        removeFormulaItem: formulaItemId => ({type: duck.types.REMOVE, payload: formulaItemId}),
         // Using thunk
         // updateWidget: widget => dispatch => {
         //     dispatch({ type: 'UPDATE', widget })
--- src/redux/modules/formulas.test.js
+++ src/redux/modules/formulas.test.js
@@ -1,6 +1,105 @@
 import formulaModule from './formulas';
 
 describe('Formula Module', () => {
-    console.log(formulaModule);
-    expect(true).toBe(true);
+	
+
+	function setup() {
+		const formulaItems = [
+			{ id: 1, name: 'General', formula: '1 + 1', value: 2, error: null, },
+			{ id: 2, name: 'Geniricic', formula: '1 + 2', value: 3, error: null, },
+			{ id: 3, name: 'Another Name', formula: '2 + 2', value: 4, error: null, },
+		];
+
+		const initialState = formulaModule.initialState;
+		const duck = formulaModule;
+		const alterData = (id) => {
+			const formula = formulaItems.find((item) => item.id === id);
+			return {
+				...formula,
+				name: `${formula.name} another`,
+				formula: `${formula.formula} + 1`,
+				value: formula.value + 1
+			};
+		};
+
+		return {
+			formulaItems,
+			initialState,
+			duck,
+			alterData
+		};
+	}
+
+	it('should return initial state', () => {
+		const { initialState, formulaItems } = setup();
+
+		const given = { type: '', payload: formulaItems.shift() };
+
+		expect(formulaModule.reducer(undefined, given, formulaModule)).toEqual(initialState);
+	});
+
+	it('should return the appropriate ADD action type and payload object', () => {
+		const { duck, formulaItems } = setup(); 
+		const expected = { type: duck.types.ADD, payload: formulaItems[0] };
+
+		const given = formulaItems[0];
+
+		expect(formulaModule.creators.addFormulaItem(given)).toEqual(expected);
+	});
+
+	it('should return the appropriate UPDATE action type and payload object', () => {
+		const { duck, formulaItems } = setup(); 
+		const expected = { type: duck.types.UPDATE, payload: formulaItems[0] };
+		const given = formulaItems[0];
+
+		expect(formulaModule.creators.updateFormulaItem(given)).toEqual(expected);
+	});
+
+	it('should return the appropriate REMOVE action type and payload object', () => {
+		const { duck, formulaItems } = setup(); 
+		const expected = { type: duck.types.REMOVE, payload: formulaItems[0].id };
+		const given = formulaItems[0].id;
+
+		expect(formulaModule.creators.removeFormulaItem(given)).toEqual(expected);
+	});
+
+	it('reducer should add a formula item', () => {
+		const { initialState, duck, formulaItems } = setup();
+		const expected = { items: [ formulaItems[0] ] };
+		const given = {
+			initialState,
+			action: duck.creators.addFormulaItem(formulaItems[0]),
+			duck
+		};
+
+		expect(formulaModule.reducer(given.initialState, given.action, given.duck)).toEqual(expected);
+	});
+
+	it('reducer should update a formula Item', () => {
+		const { duck, formulaItems, alterData, initialState } = setup();
+		const formula = formulaItems[0];
+		const alteredFormula = alterData(formula.id);
+		const expected = {...initialState, items: [ alteredFormula ].concat(formulaItems.slice(1)) };
+		const iState = {
+			items: formulaItems
+		};
+
+		const action = duck.creators.updateFormulaItem(alteredFormula);
+
+		expect(formulaModule.reducer(iState, action, duck)).toEqual(expected);
+	});
+
+	it('reducer should remove a formula Item', () => {
+		const { initialState, duck, formulaItems, alterData } = setup();
+		const formula = formulaItems[1];
+		const alteredFormula = alterData(formula.id);
+		const expected = {...initialState, items: formulaItems.slice(0, 1).concat(formulaItems.slice(2))};
+		const iState = {
+			items: formulaItems
+		};
+
+		const action = duck.creators.removeFormulaItem(formula.id);
+
+		expect(formulaModule.reducer(iState, action, duck)).toEqual(expected);
+	});
 });
